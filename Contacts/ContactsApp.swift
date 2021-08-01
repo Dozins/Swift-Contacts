494f352d9bd5e213107d3db1bddd799ecc3ccf73
Jayden <ciebiesaraningo@gmail.com>
2021-08-01 15:14:41 +0800

    first commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Jayden Lee on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
